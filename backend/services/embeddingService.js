4cfb6b920ae89b5eab0b2e2399ac47f2bea51e38
Anshukr56 <anshukr552005@gmail.com>
2026-08-19 21:58:48 +0530

    made some change

--- backend/services/embeddingService.js
+++ backend/services/embeddingService.js
@@ -5,7 +5,7 @@ const { GoogleGenAI } = require("@google/genai");
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
-
+//generating
 async function generateEmbedding(text) {
   const response = await ai.models.embedContent({
     model: "gemini-embedding-001",
